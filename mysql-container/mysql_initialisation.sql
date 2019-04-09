997333a95dadc73972ff53df1803386eb177c9f3
alphahm <ahmwebdev@gmail.com>
2019-04-09 23:11:43 +0100

    adjust password update syntax for mysql8

--- mysql-container/mysql_initialisation.sql
+++ mysql-container/mysql_initialisation.sql
@@ -6,19 +6,15 @@ SET @newuser = 'docker';
 SET @newuserpassword = 'docker';
 
 /* Set a password for root */
-SET @query = CONCAT('SET PASSWORD FOR "root"@"localhost" = PASSWORD("', @rootpassword, '")');
+SET @query = CONCAT('ALTER USER "root"@"localhost" IDENTIFIED BY "', @rootpassword, '"');
 PREPARE stmt FROM @query; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
 /* Create the new user */
-SET @query = CONCAT('
-        CREATE USER "', @newuser,'"@"172.17.0.0/255.255.0.0" IDENTIFIED BY "', @newuserpassword,'" '
-        );
+SET @query = CONCAT('CREATE USER "', @newuser,'"@"172.17.0.0/255.255.0.0" IDENTIFIED BY "', @newuserpassword,'" ');
 PREPARE stmt FROM @query; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
-
 /* Grant all privileges to the new user */
-SET @query = CONCAT('
-        GRANT ALL PRIVILEGES ON *.* TO "', @newuser, '"@"172.17.0.0/255.255.0.0" WITH GRANT OPTION');
+SET @query = CONCAT('GRANT ALL PRIVILEGES ON *.* TO "', @newuser, '"@"172.17.0.0/255.255.0.0" WITH GRANT OPTION');
 PREPARE stmt FROM @query; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
 FLUSH PRIVILEGES;
